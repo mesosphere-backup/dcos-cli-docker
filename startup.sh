129bd02387c9ff1722dbbe6ae5429f39646e34a0
Tim <timfall+github@gmail.com>
2015-09-18 13:02:09 -0700

    Fixes for ternary test.

--- startup.sh
+++ startup.sh
@@ -5,8 +5,7 @@ if [ -z $1 ]; then
   echo "Input the url of your cluster. Example:"
   echo "name.us-west-1.elb.amazonaws.com"
   echo ""
-elif [[ -z ! $1 ]]; then
-
+elif [ ! -z $1 ]; then
   sudo ./install.sh . $1
 
   echo "Setup complete."
